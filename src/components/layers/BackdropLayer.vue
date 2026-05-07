a42548a1eee9a899c55ad7a61d37723093843a62
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-05-07 09:54:39 +0300

    А

--- src/components/layers/BackdropLayer.vue
+++ src/components/layers/BackdropLayer.vue
@@ -22,14 +22,14 @@ const actualBackdropImage = computed(() => {
 	<div class="backdrop-layer">
 		<div class="backdrop__header">
 			<div class="backdrop__stripes-wrapper of-backdrop-header">
-				<Stripes class="backdrop__stripes of-backdrop-header" :angle="45" :gap-px="1.5" :stripes-px="3" width="100%" color="#000" :opacity="0.425" bg-color="#fff" />
+				<Stripes class="backdrop__stripes of-backdrop-header" :angle="45" :gap-px="1.522" :stripes-px="3" width="100%" color="#000" :opacity="0.425" bg-color="#fff" />
 			</div>
 			<div class="backdrop__stripes__overlay of-backdrop-header" />
 		</div>
 		<div class="backdrop__main-top">
 			<div class="backdrop__stripes-wrapper of-backdrop-main-top">
-				<Stripes class="backdrop__stripes of-backdrop-main-top" :angle="-45" :gap-px="3.55" width="50%" color="#fff" :opacity="0.65" />
-				<Stripes class="backdrop__stripes of-backdrop-main-top" :angle="45" :gap-px="3.55" width="50%" color="#fff" :opacity="0.65" />
+				<Stripes class="backdrop__stripes of-backdrop-main-top" :angle="-45" :gap-px="3.522" width="50%" color="#fff" :opacity="0.65" />
+				<Stripes class="backdrop__stripes of-backdrop-main-top" :angle="45" :gap-px="3.522" width="50%" color="#fff" :opacity="0.65" />
 			</div>
 			<div class="backdrop__stripes__overlay of-backdrop-main-top" />
 			<div class="backdrop__image" :style="{ '--backdrop': `url(${actualBackdropImage})` }"/>
